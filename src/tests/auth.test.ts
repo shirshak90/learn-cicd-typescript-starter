5dd19cf55ab9ea5847ee99064dc186419402fa92
shirshak197 <shirshak@ngit.com.np>
2025-12-03 18:13:57 +0545

    Add unit tests for person object in auth.test.ts

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from "vitest";
+
+const person = {
+  isActive: false,
+  age: 32,
+};
+
+describe("person", () => {
+  test("person is defined", () => {
+    expect(person).toBeDefined();
+  });
+
+  test("is active", () => {
+    expect(person.isActive).toBeTruthy();
+  });
+});
+``;
